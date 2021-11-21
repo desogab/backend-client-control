f4d187ec59add50cc10fb30705ae1ebb336c89ed
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-21 18:45:35 -0300

    [FEAT] cria rodas para criar e selecionar um profissional

--- server/data/professionalsData.js
+++ server/data/professionalsData.js
@@ -1,5 +1,13 @@
 const db = require('../infra/database');
 
 exports.getProfessionals = async function () {
-  return db.query('select * from professional_info');
-}
+  return db.query('select * from professional_user');
+};
+
+exports.saveProfessional = function (professional) {
+  return db.one('insert into professional_user (username, password) values ($1, $2) returning *', [professional.username, professional.password]);
+};
+
+exports.deleteProfessional = function (id) {
+  return db.none('delete from professional_user where id = $1', [id]);
+};
