cbfd673da0446fda51941f4f1ff64891ed987b61
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-21 18:46:29 -0300

    [FEAT] cria funcao para adicionar e editar profissional

--- server/services/professionalsService.js
+++ server/services/professionalsService.js
@@ -3,3 +3,11 @@ const professionalsData = require('../data/professionalsData');
 exports.getProfessionals = function () {
   return professionalsData.getProfessionals();
 };
+
+exports.saveProfessional = function (professional) {
+  return professionalsData.saveProfessional(professional);
+};
+
+exports.deleteProfessional = function (id) {
+  return professionalsData.deleteProfessional(id);
+}
