d5ee32dcd50eb97ab3947859dc9819a1cac33313
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-23 00:36:20 -0300

    [FEAT] cria template de teste

--- server/test/professionals.test.js
+++ server/test/professionals.test.js
@@ -1,22 +1,7 @@
 const axios = require('axios');
 const crypto = require('crypto');
-const professionalsService = require('../services/professionalsService')
-
-const generate = function () {
-  return crypto.randomBytes(20).toString('hex');
-}
+const professionalsService = require('../services/professionalsService');
 
 test('Should get posts', async function () {
-  //given - dado que
-  professionalsService.saveProfessionals({})
-  professionalsService.saveProfessionals({})
-  professionalsService.saveProfessionals({})
-  //when - quando acontecer
-  const response = await axios({
-    url: 'http://localhost:3333/api/professionals',
-    method: 'GET',
-  });
-  const professionals = response.data;
-  //then - então
 
 });
