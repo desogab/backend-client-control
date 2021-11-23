3d49c0945c299c29ee04936f2f6b2551b026e668
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-23 18:53:16 -0300

    [FEAT] create service functions for crud

--- server/services/professionalsService.js
+++ server/services/professionalsService.js
@@ -4,10 +4,18 @@ exports.getProfessionals = function () {
   return professionalsData.getProfessionals();
 };
 
+exports.getProfessional = function (id) {
+  return professionalsData.getProfessional(id);
+}
+
 exports.saveProfessional = function (professional) {
   return professionalsData.saveProfessional(professional);
 };
 
 exports.deleteProfessional = function (id) {
   return professionalsData.deleteProfessional(id);
-}
+};
+
+exports.updateProfessional = function (id, post) {
+  return professionalsData.updateProfessional(id, post);
+};
