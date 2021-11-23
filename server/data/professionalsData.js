c06acf32fdef91fbc6e966d7968ff3d2c958fa38
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-23 18:52:53 -0300

    [FEAT] create database query for crud

--- server/data/professionalsData.js
+++ server/data/professionalsData.js
@@ -4,8 +4,14 @@ exports.getProfessionals = function () {
   return db.query('select * from professional_info');
 };
 
+exports.getProfessional = function (id) {
+  return db.oneOrNone('select * from professional_info where id = $1', [id]);
+}
+
 exports.saveProfessional = function (professional) {
-  return db.query('with new_user as (insert into professional_info (name, surname, birthdate, cpf, email, phone, profession, professional_document) values ($1, $2, $3, $4, $5, $6, $7, $8) returning id) insert into professional_user (username, password, professional_id) values ($1, $2, (select id from new_user))returning *;', [
+  return db.one('with new_user as (insert into professional_user(username, password)values($1, $2)returning *) insert into professional_info(name, surname, birthdate, cpf, email, phone, profession, professional_document, user_id) values ($3, $4, $5, $6, $7, $8, $9, $10,(select id from new_user)) returning *', [
+    professional.username,
+    professional.password,
     professional.name,
     professional.surname,
     professional.birthdate,
@@ -14,11 +20,14 @@ exports.saveProfessional = function (professional) {
     professional.phone,
     professional.profession,
     professional.professional_document,
-    professional.username,
-    professional.password,
   ]);
 };
 
-exports.deleteProfessional = function () {
-  return db.none('delete ', []);
+exports.deleteProfessional = function (id) {
+  return db.none('delete from professional_user where id = $1', [id]);
 };
+
+//TODO: adicionar campos editaveis depois;
+exports.updateProfessional = function (id, professional) {
+  return db.none('update professional_info set name = $1, surname = $2 where id = $3', [professional.name, professional.surname, id]);
+}
