a1e2cce78f25ad0b180005d7033bd90b31c97817
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-23 18:52:36 -0300

    [FEAT] create all routes for crud

--- server/routes/professionalsRoute.js
+++ server/routes/professionalsRoute.js
@@ -10,16 +10,22 @@ router.get('/api/professionals', async function (req, res) {
 // router.get('/api/professionals/:id', async function (req, res) {
 
 // });
+
 router.post('/api/professionals', async function (req, res) {
   const professional = req.body;
   const createNewProfessional = await professionalsService.saveProfessional(professional);
   res.json(createNewProfessional);
 });
-// router.put('/api/professionals/:id', async function (req, res) {
 
-// });
-// router.delete('/api/professionals/:id', async function (req, res) {
+router.put('/api/professional/:id', async function (req, res) {
+  const professional = req.body;
+  await professionalsService.updateProfessional(req.params.id, professional);
+  res.end();
+});
 
-// });
+router.delete('/api/professional/:id', async function (req, res) {
+  await professionalsService.deleteProfessional(req.params.id);
+  res.end();
+});
 
 module.exports = router;
