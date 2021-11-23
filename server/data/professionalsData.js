e3106ade561749a454392f3d4fa92ba8d246e6b5
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-23 04:34:35 -0300

    [FEAT] create query to create user

--- server/data/professionalsData.js
+++ server/data/professionalsData.js
@@ -1,13 +1,24 @@
 const db = require('../infra/database');
 
-exports.getProfessionals = async function () {
-  return db.query('select * from professional_user');
+exports.getProfessionals = function () {
+  return db.query('select * from professional_info');
 };
 
 exports.saveProfessional = function (professional) {
-  return db.one('insert into professional_user (username, password) values ($1, $2) returning *', [professional.username, professional.password]);
+  return db.query('with new_user as (insert into professional_info (name, surname, birthdate, cpf, email, phone, profession, professional_document) values ($1, $2, $3, $4, $5, $6, $7, $8) returning id) insert into professional_user (username, password, professional_id) values ($1, $2, (select id from new_user))returning *;', [
+    professional.name,
+    professional.surname,
+    professional.birthdate,
+    professional.cpf,
+    professional.email,
+    professional.phone,
+    professional.profession,
+    professional.professional_document,
+    professional.username,
+    professional.password,
+  ]);
 };
 
-exports.deleteProfessional = function (id) {
-  return db.none('delete from professional_user where id = $1', [id]);
+exports.deleteProfessional = function () {
+  return db.none('delete ', []);
 };
