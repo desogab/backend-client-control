da76339461f92f6d69ea1df7e52be4e79ac91e17
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2021-11-24 02:36:11 -0300

    [REFACTOR] altera nome do arquivo

--- server/test/professional.test.js
+++ server/test/professional.test.js
@@ -10,7 +10,7 @@ const generate = function () {
 };
 //não é responsabilidade do teste
 const request = function (url, method, data) {
-  return axios({ url, method, data });
+  return axios({ url, method, data, validateStatus: false });
 };
 
 test('Should get posts', async function () {
@@ -53,6 +53,7 @@ test('Should get posts', async function () {
   });
 
   const response = await request('http://localhost:3333/api/professionals', 'get');
+  expect(response.status).toBe(200);
 
   const professionals = response.data;
   expect(professionals).toHaveLength(3);
@@ -77,12 +78,38 @@ test('Should save posts', async function () {
   };
 
 
-  const response = await request('http://localhost:3333/api/professionals', 'post', data);
+  const response = await request('http://localhost:3333/api/professional', 'post', data);
+  expect(response.status).toBe(201);
 
   const professional = response.data;
 
   expect(professional.name).toBe(data.name);
   expect(professional.surname).toBe(data.surname);
+  await professionalsService.deleteProfessional(professional.id);
+});
+
+test('Should not save posts', async function () {
+
+  const data = {
+    name: generate(),
+    surname: generate(),
+    birthdate: '10-10-1010',
+    cpf: generate(),
+    email: generate(),
+    phone: generate(),
+    profession: 'PSICOLOGA',
+    professional_document: generate(),
+    username: generate(),
+    password: generate()
+  };
+
+
+  const response1 = await request('http://localhost:3333/api/professional', 'post', data);
+  const response2 = await request('http://localhost:3333/api/professional', 'post', data);
+  expect(response2.status).toBe(409);
+  expect(response1.status).toBe(201);
+
+  const professional = response1.data;
   await professionalsService.deleteProfessional(professional.id);
 });
 
@@ -102,13 +129,23 @@ test('Should update posts', async function () {
   professional.name = generate();
   professional.surname = generate();
 
-  await request(`http://localhost:3333/api/professional/${professional.id}`, 'put', professional);
+  const response = await request(`http://localhost:3333/api/professional/${professional.id}`, 'put', professional);
+  expect(response.status).toBe(204);
 
   const updatedProfessional = await professionalsService.getProfessional(professional.id);
 
   expect(updatedProfessional.name).toBe(professional.name);
   expect(updatedProfessional.surname).toBe(professional.surname);
   await professionalsService.deleteProfessional(professional.id);
+});
+
+test('Should not update posts', async function () {
+  const professional = {
+    id: 1
+  };
+
+  const response = await request(`http://localhost:3333/api/professional/${professional.id}`, 'put', professional);
+  expect(response.status).toBe(404);
 });
 
 test('Should delete posts', async function () {
@@ -125,7 +162,8 @@ test('Should delete posts', async function () {
     password: generate()
   });
 
-  await request(`http://localhost:3333/api/professional/${professional.id}`, 'delete');
+  const response = await request(`http://localhost:3333/api/professional/${professional.id}`, 'delete');
+  expect(response.status).toBe(204);
   const professionals = await professionalsService.getProfessionals();
   expect(professionals).toHaveLength(0);
 });
