feaf01577d54f6384c82d9a3166baa4fdbc8e4d3
Gabriel Gomes <gabrielgoomes.desouza@gmail.com>
2021-11-25 20:43:41 -0300

    [FIX] Remove enum type das tabelas consultation_info e professional_info

--- database/create.sql
+++ database/create.sql
@@ -9,13 +9,13 @@ CREATE TABLE "professional_info"(
   name VARCHAR(50) NOT NULL,
   surname VARCHAR(100) NOT NULL,
   birthdate DATE NOT NULL,
-  cpf char(14) UNIQUE NOT NULL,
+  cpf VARCHAR(14) UNIQUE NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
   phone VARCHAR(15) UNIQUE NOT NULL,
-  profession PROFESSION NOT NULL,
+  profession VARCHAR(50) NOT NULL,
   professional_document VARCHAR(50) UNIQUE NOT NULL,
   created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP(3) NOT NULL,
+  updated_at TIMESTAMP(3),
   user_id INTEGER UNIQUE NOT NULL,
   FOREIGN KEY (user_id) REFERENCES "professional_user"(id)
 );
@@ -27,12 +27,12 @@ CREATE TABLE "client"(
   name VARCHAR(50) NOT NULL,
   surname VARCHAR(100) NOT NULL,
   birthdate DATE NOT NULL,
-  cpf char(14) UNIQUE NOT NULL,
+  cpf VARCHAR(14) UNIQUE NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
   phone VARCHAR(15) UNIQUE NOT NULL,
   consultation_price MONEY NOT NULL,
   created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP(3) NOT NULL,
+  updated_at TIMESTAMP(3),
   professional_id INTEGER NOT NULL UNIQUE,
   FOREIGN KEY (professional_id) REFERENCES "professional_info"(id)
 );
@@ -41,7 +41,7 @@ CREATE TABLE "consultation_info" (
   id SERIAL PRIMARY KEY,
   date_visit DATE NOT NULL,
   hours_visit TIME NOT NULL,
-  model CONSULTATION_MODEL,
+  model VARCHAR(15),
   professional_id INTEGER UNIQUE NOT NULL,
   client_id INTEGER UNIQUE NOT NULL,
   FOREIGN KEY (professional_id) REFERENCES "professional_info"(id),
@@ -54,7 +54,7 @@ CREATE TABLE "client_sponsor"(
   surname VARCHAR(100) NOT NULL,
   cpf VARCHAR(14) UNIQUE NOT NULL,
   created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP(3) NOT NULL,
+  updated_at TIMESTAMP(3),
   sponsor_id INTEGER UNIQUE,
   FOREIGN KEY (sponsor_id) REFERENCES "client"(id)
 );
@@ -65,7 +65,7 @@ CREATE TABLE "client_emergency"(
   surname VARCHAR(100) NOT NULL,
   phone VARCHAR(15) NOT NULL,
   created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP(3) NOT NULL,
+  updated_at TIMESTAMP(3),
   client_id INTEGER UNIQUE NOT NULL,
   FOREIGN KEY (client_id) REFERENCES "client"(id)
 );
@@ -80,7 +80,7 @@ CREATE TABLE "client_address"(
   state char(2) NOT NULL,
   zipcode VARCHAR(25) NOT NULL,
   created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP(3) NOT NULL,
+  updated_at TIMESTAMP(3),
   client_id INTEGER NOT NULL,
   FOREIGN KEY (client_id) REFERENCES "client"(id)
 );
