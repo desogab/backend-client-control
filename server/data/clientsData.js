577d07ad1473e7d2ad6f4ba5dfb68328dd79fc60
Gabriel Gomes <gabrielgoomes.desouza@gmail.com>
2021-11-26 18:57:58 -0300

    [FEAT] cria query para novo cliente na camada data e servico

--- server/data/clientsData.js
+++ server/data/clientsData.js
@@ -1,3 +1,39 @@
 const db = require('../infra/database');
 
-exports.saveClient = function (client) {};
+exports.saveClient = async function (client) {
+  return await db.txIf(async function (t) {
+    const newClient = await t.one(
+      'WITH new_client AS (INSERT INTO client(active, sponsor, name, surname, birthdate, cpf, email, phone, consultation_price, professional_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id), new_address AS (INSERT INTO client_address(street, district, number, city, complement, state, zipcode, client_id) VALUES ($11,$12,$13,$14,$15,$16,$17,(SELECT id FROM new_client)) RETURNING client_id), new_emergency AS (INSERT INTO client_emergency (name, surname, phone, client_id) VALUES ($18, $19, $20, (SELECT client_id FROM new_address))RETURNING client_id) INSERT INTO client_sponsor (name, surname, cpf, client_id) VALUES ($21, $22, $23, (SELECT client_id FROM new_emergency)) RETURNING *',
+      [
+        client.active,
+        client.sponsor,
+        client.name,
+        client.surname,
+        client.birthdate,
+        client.cpf,
+        client.email,
+        client.phone,
+        client.consultation_price,
+        client.professional_id,
+        client.street,
+        client.district,
+        client.number,
+        client.city,
+        client.complement,
+        client.state,
+        client.zipcode,
+        client.emergency_name,
+        client.emergency_surname,
+        client.emergency_phone,
+        client.sponsor_name,
+        client.sponsor_surname,
+        client.sponsor_cpf
+      ]
+    );
+    return t.one('select * from client where id = $1', [newClient.client_id]);
+  });
+};
+
+exports.deleteClient = function (id) {
+  return db.none('delete from client where id = $1', [id]);
+};
--- server/services/clientsService.js
+++ server/services/clientsService.js
@@ -1,1 +1,9 @@
 const clientsData = require('../data/clientsData');
+
+exports.saveClient = function (client) {
+  return clientsData.saveClient(client);
+};
+
+exports.deleteClient = function (id) {
+  return clientsData.deleteClient(id);
+};
