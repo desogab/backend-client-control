9230457cfbac476d429062ceba82113a266f52ad
Gabriel Gomes <gabrielgoomes.desouza@gmail.com>
2022-01-08 03:39:13 -0300

    [FEAT] create auth jwt

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
+const jwt = require('express-jwt');
 const app = express();
 
 app.use(express.json());
+app.use(
+  '/',
+  jwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }).unless({
+    path: ['/api/auth'],
+  })
+);
 
 app.use(require('./routes/professionalsRoute'));
 app.use(require('./routes/clientsRoute'));
+app.use(require('./routes/auth'));
 
 app.use(function (error, req, res, next) {
-  if (error.message === 'Professional already exists') {
+  if (error.message === 'Already exists') {
     return res.status(409).send(error.message);
   }
-  if (error.message === 'Professional not found') {
+  if (error.message === 'Not found') {
     return res.status(404).send(error.message);
   }
-  res.status(500).send(error.message);
+  // res.status(500).send(error.message);
 });
 
 const PORT = 3333;
