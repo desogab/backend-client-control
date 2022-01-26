5ef1f46ce8a2f33a259a7248296ef7f2b2637a63
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-01-26 02:15:15 -0300

    [REMOVE] remove pastas que serao implementadas depois futuramente

--- src/data/authData.ts
+++
@@ -1,8 +0,0 @@
-import db from '../infra/database'
-import { IProfessionalUser } from 'src/@types/professional'
-export const authProfessional = (username:IProfessionalUser['username'], password:IProfessionalUser['password']) => db.oneOrNone(
-  'select pi2.id from professional_info pi2 join professional_user pu on pi2.user_id = pu.id where pu.username = $1 and pu."password" = $2',
-  [username, password]
-)
-
-export default authProfessional
